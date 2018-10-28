80920bc1790e067a1105625f87b64cd462597774
Edson Neri <edson.neri@correo.uia.mx>
2018-10-28 13:16:46 -0600

    refactors `indexjs`
      - To remove `require` in favor of `import`
      - To use `schema.js` and `resolvers.js`
    
    closes #1

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const express = require('express');
-const {
-  ApolloServer,
-  gql
-} = require('apollo-server-express');
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import resolvers from './resolvers';
+import schema from './schema';
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ schema, resolvers });
 
 const app = express();
 server.applyMiddleware({ app });
