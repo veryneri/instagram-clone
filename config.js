8040113417e4d45c1d3b2e4f56f91f81e801a6f0
Edson Neri <edson.neri@correo.uia.mx>
2019-06-26 00:18:40 -0500

    adds `mongoose` package, config and connection
    closes #2

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  db: {
+    port: '27017',
+    schema: 'instagram-clone',
+    server: 'localhost',
+  },
+}
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import config from './config';
+import mongoose from 'mongoose';
 import resolvers from './resolvers';
 import typeDefs from './schema';
+
+mongoose.Promise = global.Promise;
+const {
+  db: {
+    port: dbPort,
+    schema: dbSchema,
+    server: dbServer,
+  },
+} = config;
+const mongoUri = `mongodb://${dbServer}:${dbPort}/${dbSchema}`;
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -10,7 +22,12 @@ server.applyMiddleware({ app });
 
 const port = 4000;
 
-app.listen({ port }, () =>
+mongoose.connect(mongoUri, { useNewUrlParser: true }).then(() => {
   /* eslint-disable-next-line no-console */
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
-);
+  console.log('DB connection established');
+
+  app.listen({ port }, () =>
+    /* eslint-disable-next-line no-console */
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+  );
+});
